c683ea78ba645a14eb59c62c1f3ac7e13818933c
Miftakhul Ilman Rifqi <bozzclax@gmail.com>
2025-07-23 13:57:40 +0700

    Remove performance check controller and update ping message

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -2,7 +2,7 @@ import { getRuntime } from "../utils/runtime.util";
 
 export const ping = () => {
     return {
-        message: "Halo! Ini API sederhana dengan Elysia untuk ping",
+        message: "Halo! Ini API sederhana dengan Elysia untuk PING",
         runtime: getRuntime(),
         timestamp: new Date().toISOString(),
     };
--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -1,11 +1,9 @@
 import { Elysia } from "elysia";
 import { ping } from "../controllers/ping.controller";
-import { checkPerformance } from "../controllers/check.controller";
 import { runtimeInfo } from "../controllers/runtime.controller";
 
 const apiRoute = new Elysia({ prefix: "/api/v1" })
     .get("/ping", () => ping())
-    .get("/check", () => checkPerformance())
     .get("/runtime", () => runtimeInfo());
 
 export default apiRoute;
